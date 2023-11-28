9f0653b4bcade8a619c781ec997bd78492c563b2
danilonunezgil <danilo.gil98@gmail.com>
2023-11-28 18:37:45 -0500

    spring boot project added

---
+++ src/main/java/com/example/camunda/serviceTask/TweetContentOfflineDelegate.java
@@ -0,0 +1,16 @@
+package com.example.camunda.serviceTask;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Service;
+
+@Service("tweetAdapter")
+public class TweetContentOfflineDelegate implements JavaDelegate {
+    public void execute(DelegateExecution execution) throws Exception {
+        String content = (String) execution.getVariable("content");
+
+        System.out.println("\n\n\n######\n\n\n");
+        System.out.println("NOW WE WOULD TWEET: '" + content + "'");
+        System.out.println("\n\n\n######\n\n\n");
+    }
+}
